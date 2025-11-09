4f64c2810b1ee78386a79cb01b31bcfd96a398e0
Amen1412 <anonyamen@gmail.com>
2025-11-09 22:21:58 +0400

    Use environment variable for server port

--- app.py
+++ app.py
@@ -149,4 +149,5 @@ def refresh():
 fetch_and_cache_movies()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=7000)
+    port = int(os.getenv('PORT', 7000))
+    app.run(host="0.0.0.0", port=port)
